07a0e16a42ede1d955127f86989e6b3d018e09f0
Komaromi Gyorgy <komaromigy40@gmail.cm>
2020-07-29 10:57:49 +0300

    add base components

---
+++ src/views/Menu.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-container>
+    <v-row v-for="(drink, i) in menu.drinks" :key="i">
+      <p>{{drink.name}}</p>
+      <img
+        :src="drink.image"
+      />
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import drinkList from '@/assets/drinks.json'
+
+export default {
+  name: 'PageHome',
+  data () {
+    return {
+      menu: drinkList
+    }
+  }
+}
+</script>>
